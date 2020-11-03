11cecdaede8b13a4dea5804cffd8214a0ae451d6
Nicolas Saudrais <nicolas.saudrais@athennian.com>
2020-11-03 16:44:17 -0500

    replace bignum with big-integer lib for better node versions compatibility

--- src/payloadBuilder.js
+++ src/payloadBuilder.js
@@ -1,4 +1,23 @@
-const bignum = require("bignum");
+const BigInteger = require('big-integer');
+
+// taken from https://stackoverflow.com/questions/48521840/biginteger-to-a-uint8array-of-bytes
+const zero = BigInteger(0);
+const n256 = BigInteger(256);
+
+function toLittleEndian(bigNumber) {
+    let result = new Uint8Array(8);
+    let i = 0;
+    while (bigNumber.greater(zero)) {
+        result[i] = bigNumber.mod(n256);
+        bigNumber = bigNumber.divide(n256);
+        i += 1;
+    }
+    return result;
+}
+
+function toBigEndian(bytes) {
+    return toLittleEndian(bytes).reverse();
+}
 
 module.exports = {
     build: function(input) {
@@ -7,10 +26,7 @@ module.exports = {
         const fullUrlBuffer = Buffer.from(fullUrl, "utf8");
         const accessTokenBuffer = Buffer.from(input.accessToken, "utf8");
 
-        const timeBuffer = bignum(input.timestamp).toBuffer({
-            endian: "big",
-            size: 8
-        });
+        const timeBuffer = toBigEndian(BigInteger(input.timestamp))
 
         const expectedProofBuffer = Buffer.concat([
             getLengthIn4Bytes(accessTokenBuffer),
